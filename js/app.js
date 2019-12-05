6cd994c459cb6bbb85d584a3d9e68369f45487e2
Hasan Opiev <hasanopiev@gmail.com>
2019-12-05 23:18:11 +0000

    Retrieve searched users repo information and console logged

--- js/app.js
+++ js/app.js
@@ -13,18 +13,38 @@ const fetchUsers = async(user) => {
     const data = await api_call.json();
     return { data }
 };
+const getRepos = async(user) => {
+    const repo_call = await fetch(`https://api.github.com/users/${user}/repos?client_id =${client_id}&client_secret=${client_secret}`);
+
+    const repos = await repo_call.json();
+    return{ data : repos }
+};
+
+
+// async function getRepos(){
+//     clear();
+//     const url ="https:/api.github.com/search/repositories?q=stars:>100000"
+//     // const response = await fetch(url)
+//     // const result = await response.json()
 
 const showData = () => {
     fetchUsers(inputValue.value).then((res) => {
-        console.log(res);    
-
+        console.log(res);   
+        
         nameContainer.innerHTML = `Name: <span class="main_profile-value">${res.data.name}</span>`;
         unContainer.innerHTML = `Username: <span class="main_profile-value">${res.data.login}</span>`;
         // imageContainer.innerHTML = `<img class="main_profile-value" src=${res.data.avatar_url}/>`;
-        
+
+    })
+};
+
+const showRepos = () => {
+    getRepos(inputValue.value).then((repo) => {
+        console.log(repo); 
     })
 };
 
 searchButton.addEventListener("click", () => {
     showData();
+    showRepos();
 })
