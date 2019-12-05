666b45085f8c82fac513c4b07ff89e606e564df4
Hasan Opiev <hasanopiev@gmail.com>
2019-12-05 18:50:02 +0000

    retrieve some data from API and display on screen

--- js/app.js
+++ js/app.js
@@ -1,13 +1,26 @@
 const inputValue = document.querySelector("#search");
 const searchButton = document.querySelector(".searchButton");
+const nameContainer = document.querySelector(".profile-name");
+const unContainer = document.querySelector(".profile-username");
 
 const client_id = "565b0b7aa0c9c7eafb3c";
 const client_secret = "ead17169a57c3ce0bc6663c9280a22d61d22d37e";
 
 const fetchUsers = async(user) => {
     const api_call = await fetch(`https://api.github.com/users/${user}?client_id =${client_id}&client_secret=${client_secret}`);
-}
+
+    const data = await api_call.json();
+    return { data }
+};
+
+const showData = () => {
+    fetchUsers(inputValue.value).then((res) => {
+        console.log(res);    
+
+        nameContainer.innerHTML = `Name: <span class="main_profile-value">${res.data.name}</span>`;
+    })
+};
 
 searchButton.addEventListener("click", () => {
-    console.log("hello");
+    showData();
 })
